94fbd932a6b12789476a4b7dc24a9354d8e0d4e2
atahmaz <aycatahmaz@gmail.com>
2021-05-27 15:16:48 +0300

    hrms project has been created.

---
+++ hrms/hrms/src/main/java/kodlamaio/hrms/api/controllers/JobTitlesControllers.java
@@ -0,0 +1,31 @@
+package kodlamaio.hrms.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.JobTitleService;
+import kodlamaio.hrms.entities.concretes.JobTitle;
+
+@RestController
+@RequestMapping("/api/job_titles")
+public class JobTitlesControllers {
+	
+	private  JobTitleService jobTitleService;
+	
+	@Autowired
+	public JobTitlesControllers(JobTitleService jobTitleService) {
+		super();
+		this.jobTitleService = jobTitleService;
+	}
+
+	@GetMapping("/getAll")
+	public List<JobTitle> getAll(){
+		
+		return this.jobTitleService.getAll();
+	}
+
+}
